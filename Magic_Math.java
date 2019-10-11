a50c6a679afb727d4a6200d6779b54090e1164b8
Renan silva maciel <49588952+renansilvamaciel@users.noreply.github.com>
2019-10-11 13:26:03 -0300

    Add files via upload

---
+++ Magic_Math.java
@@ -0,0 +1,24 @@
+
+package magic_math;
+
+/**
+ *
+ * @author renan.smaciel
+ * 
+ *             ****** ATENCAO ******
+ *  Sempre lembre de comentar o codgo, quando criar alguma cariavel ou funcao 
+ * lembre-se sempre que tem que ser entendido por todos do grupo
+ * se possivel faca uma breve descricao do que a funcao faz e quando 
+ * criar variavel indique para que ela serve;
+ * 
+ */
+public class Magic_Math 
+{
+
+     
+    public static void main(String[] args) 
+    {
+      
+    }
+    
+}
