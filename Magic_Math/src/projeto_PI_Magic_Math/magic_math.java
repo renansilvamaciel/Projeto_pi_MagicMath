77cc126073fafcd3303ab06733e734981fa614b3
I <I@192.168.33.206>
2019-10-12 02:32:09 -0300

    alteracao no diretorio add um projeto inteiro ao diretorio

--- Magic_Math/src/projeto_PI_Magic_Math/magic_math.java
+++ Magic_Math/src/projeto_PI_Magic_Math/magic_math.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * @author renan.smaciel
+ * 
+ *             ****** ATENCAO ******
+ *  Sempre lembre de comentar o codgo, quando criar alguma cariavel ou funcao 
+ * lembre-se sempre que tem que ser entendido por todos do grupo
+ * se possivel faca uma breve descricao do que a funcao faz e quando 
+ * criar variavel indique para que ela serve;
+ * 
  */
 package projeto_PI_Magic_Math;
+
+import java.util.Scanner;
 
 /**
  *
  * @author I
  */
-public class magic_math {
-    
+public class magic_math 
+{
+     static Scanner entrar = new Scanner(System.in);//objeto criado para receber dados que serão armazenados em variaveis
+     public static void main(String[] args) 
+    {
+        System.out.println("Salve quebrada!");
+    }
 }
+
+    
+   
+
+     
+   
+    
+
