5d1c9bf0114a4ada1d19e76d7cce1017d70bcda1
MarcosDead <>
2019-10-13 12:27:02 -0300

    Corpo do menu definido

--- Magic_Math/src/projeto_PI_Magic_Math/magic_math.java
+++ Magic_Math/src/projeto_PI_Magic_Math/magic_math.java
@@ -19,12 +19,122 @@ import java.util.Scanner;
  */
 public class magic_math 
 {
-     static Scanner entrar = new Scanner(System.in);//objeto criado para receber dados que serão armazenados em variaveis
+     static int opcoesMenu(int op){ // opções ao escolher uma das alternativas do menu inicial
+        
+        
+        switch(op){
+            case 1:
+                System.out.println("                                             _______________________\n" +
+"   _______________________-------------------                       `\\\n" +
+" /:--__                                                              |\n" +
+"||< > |                                   ___________________________/\n" +
+"| \\__/_________________-------------------                         |\n" +
+"|                                                                  |\n" +
+" |                             MAGIC MATH                          |\n" +
+" |                                                                  |\n" +
+" |      \"Assim como toda historia tem um começo está não            |\n" +
+"  |        seria diferente, A nossa historia se passa em algum       |\n" +
+"  |      seculo jás perdido em meio as ruinas do tempo,              |\n" +
+"  |      em uma terra média chamada Eldia quando a magia e o irreal  |\n" +
+"  |      ainda coexistiam, a partir deste ponto temos                 |\n" +
+"   |      um jovem rapaz chamado ..., que em poucos dias ele          |\n" +
+"   |       começaria uma jornada que mudaria a sua vida,              |\n" +
+"   |     a vida neste seculo não é nada facil, toda Eldia está       |\n" +
+"  |                                                                   |\n" +
+" |                                                                  |\n" +
+" |      \"destruida há seculos, a historia e todo o conhecimento    |\n" +
+"  |        contido em nosso mundo está escasso, a busca por uma      |\n" +
+"  |      coordenada que levara até a verdade sobre o nosso mundo é   |\n" +
+"  |       constante e no meio disto a nação soberana de Marven quer  |\n" +
+"  |      impedir que alguem consiga obter tais conhecimentos,         |\n" +
+"   |      somente obetendo tais informações poderá reconstruir o      |\n" +
+"   |      mundo novamente e você é o escolhido para restaurar a paz   |\n" +
+"   |         e a ordem em nosso mundo, você aceita continuar?        |\n" +
+"  |                                              ____________________|_ \n" +
+"  |  ___________________-------------------------                      `\\\n" +
+"  |/`--_                  ╔════•ೋೋ•════╗                    |\n" +
+"  ||[ ]||                   1- [Sim]   || 2- [Não]   ___________________/\n" +
+"   \\===/___________________--------------------------");
+             
+                //COMEÇO DO JOGO A PARTIR DA DECISÃO SIM
+                
+                int dados = entrarDadosInt();
+                
+                if (dados == 1){
+                    System.out.println("Informe o seu nome: ");
+                  String nome =  entrarDadosStr();
+                    System.out.println("olá, caro jovem "+nome+", eu sou o 13º mago celestial o ultimo de todos, seu pai foi um grande mago, um excelente matematico,\n" +
+" amigo e professor e ele confiou a mim a missão de guia-lo em seu destino, sua jornada se passara nas ruínas do mundo desconhecido\n" +
+" onde caso você consiga passar pelos 10 desafios divinos você obtera o conhecimento supremo para enfim restaurar o nosso mundo,\n" +
+" o povo de Eldia conta com você, salve o mundo e construa o seu destino...");
+                }
+                
+                else
+                    System.out.println("Saindo do jogo...");
+                break;
+                        
+                
+                
+                
+            case 2:
+                System.out.println("╔════•ೋೋ•════╗ \n" +
+                                   "         Historias\n" +
+                                   "╚════•ೋೋ•════╝");
+                break;
+                
+                
+                
+                
+                
+            case 3:
+                System.out.println("╔════•ೋೋ•════╗ \n" +
+                                   "         Créditos\n" +
+                                   "╚════•ೋೋ•════╝");
+                
+                System.out.println("Trabalho Projeto Integrador (P.I)");
+                System.out.println("Turma 1ºA TADS - Noturno, 1º Semestre, 2019");
+                System.out.println("Desenvolvido por Marcos vinicius Santos Souza, Renan Maciel e Matheus");
+                break;
+                
+                
+                
+                
+            case 4:
+                System.out.println("Saindo do jogo...");
+                break;
+                
+        }
+        
+        return op;
+    }
+    
      
-     static void cabecalhoo()// cabeçalho do programa
-     { 
-         
-         System.out.println("╔════•ೋೋ•════╗ \n" +
+     
+     
+     
+   static int entrarDadosInt(){ // captura os dados de numeros inteiros para salvar em variaveis
+      Scanner entrar = new Scanner(System.in);
+      int dados = entrar.nextInt();
+      return dados;
+   }
+   
+   static String entrarDadosStr(){ // captura os dados tipo Strings para salvar em variaveis
+      Scanner entrar = new Scanner(System.in);
+      String dados = entrar.next();
+      return dados;
+   }
+    
+   
+   
+   
+    
+    public static void main(String[] args) { // Corpo do programa
+        
+        Scanner entrar = new Scanner(System.in);
+        
+        int opcao;
+        
+        System.out.println("╔════•ೋೋ•════╗ \n" +
                             "         Magic Math\n" +
                             "╚════•ೋೋ•════╝");
          System.out.println("        ︻雷魔▅▆▇◤");
@@ -36,16 +146,9 @@ public class magic_math
                             "╲▂▂╱╭╯╱▔▔╱▔▔▔▽▽╯\n" +
                             "┊╱╱╭╯╱▔▔▔╲▂▂△▂△╮\n" +
                             "━━━╯╱╱╭━━━━━━━━╯");
-        
          
-        
-     }
-     
-     
-     static int menuInicial(){ //tela(menu) inicial do jogo
-        int opcaoEntrar;
-        
-        System.out.println("");
+         
+         System.out.println("");
         System.out.println("╔════•ೋೋ•════╗ \n" +
                             "            Menu\n" +
                            "╚════•ೋೋ•════╝");
@@ -66,74 +169,15 @@ public class magic_math
 "      ((+) .  .:)\n" +
 "      |`.-(o)-.'|\n" +
 "      \\/  \\_/  \\/         ");
-                    
-        
-        opcaoEntrar = entrar.nextInt();
-        
-        return opcaoEntrar;
-    }
-     
-     
-     
-     static int opcoesMenu(int op){ // opções ao escolher uma das alternativas do menu inicial
-        
-        
-        switch(op){
-            case 1:
-                System.out.println("                                             _________________________\n" +
-"   _______________________-------------------                         `\\\n" +
-" /:--__                                                                |\n" +
-"||< > |                                     ___________________________/\n" +
-"| \\__/_________________-------------------                           |\n" +
-"|                                                                    |\n" +
-" |                              Magic Math                           |\n" +
-" |                                                                    |\n" +
-" | \"Assim como toda historia tem um começo está não seria diferente,|\n" +
-"  |  A nossa historia se passa em algum seculo jás perdido           |\n" +
-"  | em meio as ruinas do tempo, em uma terra média chamada Eldia      |\n" +
-"  | quando a magia e o irreal ainda coexistiam, a partir deste ponto   |\n" +
-"  |  temos um jovem rapaz chamado ..., que em poucos dias ele começaria |\n" +
-"   | uma jornada que mudaria a sua vida, a vida neste seculo não é nada |\n" +
-"   | facil, toda Eldia está destruida há seculos, a historia e todo o   |\n" +
-"   | conhecimento contido em nosso mundo está escasso, a busca por uma |\n" +
-"  |  coordenada que levara até a verdade sobre o  ____________________|_\n" +
-"  |  ___________________-------------------------                        `\\\n" +
-"  |/`--_                                                                   |\n" +
-"  ||[ ]||                                              ___________________/\n" +
-"   \\===/___________________--------------------------");
-                break;
-                        
-            case 2:
-                System.out.println("Historia");
-                break;
-                
-            case 3:
-                System.out.println("╔════•ೋೋ•════╗ \n" +
-                                   "         Créditos\n" +
-                                   "╚════•ೋೋ•════╝");
-                
-                System.out.println("Trabalho Projeto Integrador (P.I)");
-                System.out.println("Turma 1ºA TADS - Noturno, 1º Semestre");
-                System.out.println("Desenvolvido por Marcos vinicius Santos Souza, Renan Maciel e Matheus");
-                break;
-                
-            case 4:
-                System.out.println("Saindo do jogo...");
-                break;
-                
-        }
-        
-        return menuInicial();
-    }
-     
-     
-     public static void main(String[] args) 
-    {
-       cabecalhoo();
-       int menu = menuInicial();  // criando variavel para receber a entrada dos dados (escolha) do menu
+            
+            
+            
+       opcao = entrar.nextInt(); // capturando dados
        
-       int opM = opcoesMenu(menu);
+       int opMenu = opcoesMenu(opcao); // criando variavel que recebera os dados coletados e passara para a função switch "opcoesMenu"
+                    
     }
+    
 }
 
     
